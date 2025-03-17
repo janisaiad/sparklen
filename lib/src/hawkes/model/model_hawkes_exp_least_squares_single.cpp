a29728810fb5fbeb36b6d6e3e42ee920235de1e9
romain-e-lacoste <143622132+romain-e-lacoste@users.noreply.github.com>
2025-03-17 11:32:08 +0100

    Update pre-calculation

--- lib/src/hawkes/model/model_hawkes_exp_least_squares_single.cpp
+++ lib/src/hawkes/model/model_hawkes_exp_least_squares_single.cpp
@@ -9,53 +9,25 @@ ModelHawkesExpLeastSquaresSingle::ModelHawkesExpLeastSquaresSingle(){
 	n_components = 0;
 	weights_computed = false;
 	N = ArrayInt1D(n_components);
-	H = ArrayDouble1D(n_components);
-	H2 = ArrayDouble1D(n_components);
-	D = ArrayDouble2D(n_components, n_components);
-	C = ArrayDouble2D(n_components, n_components);
+	I = ArrayDouble1D(n_components);
+	I2 = ArrayDouble1D(n_components);
+	V = ArrayDouble2D(n_components, n_components);
+	W = ArrayDouble2D(n_components, n_components);
 }
 
 ModelHawkesExpLeastSquaresSingle::ModelHawkesExpLeastSquaresSingle(size_t n){
 	n_components = n;
 	weights_computed = false;
 	N = ArrayInt1D(n_components);
-	H = ArrayDouble1D(n_components);
-	H2 = ArrayDouble1D(n_components);
-	D = ArrayDouble2D(n_components, n_components);
-	C = ArrayDouble2D(n_components, n_components);
+	I = ArrayDouble1D(n_components);
+	I2 = ArrayDouble1D(n_components);
+	V = ArrayDouble2D(n_components, n_components);
+	W = ArrayDouble2D(n_components, n_components);
 }
 
 size_t ModelHawkesExpLeastSquaresSingle::get_n_components(){
 	return n_components;
 }
-
-/* void ModelHawkesExpLeastSquaresSingle::compute_weights(const ListSharedArrayDouble1D &jump_times, const double end_time, const double decay){
-
-	for (size_t j=0; j<n_components; ++j){
-		const SharedArrayDouble1D &jump_times_j = jump_times[j];
-		N[j] = jump_times_j.size();
-		for (size_t j2=0; j2<n_components; ++j2){
-			const SharedArrayDouble1D &jump_times_j2 = jump_times[j2];
-			for (size_t h=0; h<jump_times_j.size(); ++h){
-				size_t h2 = 0;
-				double term = 0.;
-				while (h2 < jump_times_j2.size() && jump_times_j2[h2] <jump_times_j[h]){
-					term += decay*(exp(-decay*(jump_times_j[h]-jump_times_j2[h2])));
-					h2++;
-				}
-				D(j,j2) += term;
-				C(j,j2) += (1-exp(-2*decay*(end_time-jump_times_j[h])))*term;
-			}
-		}
-		for (size_t h=0; h<jump_times_j.size(); ++h){
-			H[j] += 1-exp(-decay*(end_time-jump_times_j[h]));
-			H2[j] += 1-exp(-2*decay*(end_time-jump_times_j[h]));
-		}
-		H2[j] *= decay/2;
-	}
-
-	weights_computed = true;
-} */
 
 void ModelHawkesExpLeastSquaresSingle::compute_weights(const ListSharedArrayDouble1D &jump_times, const double end_time, const double decay){
 
@@ -74,48 +46,19 @@ void ModelHawkesExpLeastSquaresSingle::compute_weights(const ListSharedArrayDoub
 					G += decay*(exp(-decay*(jump_times_j[h]-jump_times_j2[h2])));
 					h2++;
 				}
-				D(j,j2) += G;
-				C(j,j2) += (1-exp(-2*decay*(end_time-jump_times_j[h])))*G;
+				V(j,j2) += G;
+				W(j,j2) += (1-exp(-2*decay*(end_time-jump_times_j[h])))*G;
 			}
 		}
 		for (size_t h=0; h<jump_times_j.size(); ++h){
-			H[j] += 1-exp(-decay*(end_time-jump_times_j[h]));
-			H2[j] += 1-exp(-2*decay*(end_time-jump_times_j[h]));
+			I[j] += 1-exp(-decay*(end_time-jump_times_j[h]));
+			I2[j] += 1-exp(-2*decay*(end_time-jump_times_j[h]));
 		}
-		H2[j] *= decay/2;
+		I2[j] *= decay/2;
 	}
 
 	weights_computed = true;
 }
-
-/* void ModelHawkesExpLeastSquaresSingle::compute_weights(const ListSharedArrayDouble1D &jump_times, const double end_time, const double decay){
-
-	for (size_t j=0; j<n_components; ++j){
-		const SharedArrayDouble1D &jump_times_j = jump_times[j];
-		N[j] = jump_times_j.size();
-		SharedArrayDouble1D G(n_components);
-		ArrayInt1D h2 (n_components);
-		for (size_t h=0; h<jump_times_j.size(); ++h){
-			for (size_t j2=0; j2<n_components; ++j2){
-				const SharedArrayDouble1D &jump_times_j2 = jump_times[j2];
-				if (h > 0) {
-					G[j2] *= exp(-decay*(jump_times_j[h]-jump_times_j[h-1]));
-				}
-				while (h2[j2] < jump_times_j2.size() && jump_times_j2[h2[j2]] <jump_times_j[h]){
-					G[j2] += decay*(exp(-decay*(jump_times_j[h]-jump_times_j2[h2[j2]])));
-					h2[j2]++;
-				}
-				D(j,j2) += G[j2];
-				C(j,j2) += (1-exp(-2*decay*(end_time-jump_times_j[h])))*G[j2];
-			}
-			H[j] += 1-exp(-decay*(end_time-jump_times_j[h]));
-			H2[j] += 1-exp(-2*decay*(end_time-jump_times_j[h]));
-		}
-		H2[j] *= decay/2;
-	}
-
-	weights_computed = true;
-} */
 
 double ModelHawkesExpLeastSquaresSingle::compute_loss_i(const size_t i, const ListSharedArrayDouble1D &jump_times, const double end_time, const double decay, const SharedArrayDouble2D &theta){
 
@@ -129,11 +72,11 @@ double ModelHawkesExpLeastSquaresSingle::compute_loss_i(const size_t i, const Li
 	ls_contrast_value_i += theta(i,0)*theta(i,0)*end_time - 2*theta(i,0)*N[i];
 
 	for (size_t j=0; j<n_components; ++j){
-		term_square += theta(i,j+1)*theta(i,j+1)*H2[j];
-		term_cross_mu += theta(i,j+1)*H[j];
-		term_stieltjes += theta(i,j+1)*D(i,j);
+		term_square += theta(i,j+1)*theta(i,j+1)*I2[j];
+		term_cross_mu += theta(i,j+1)*I[j];
+		term_stieltjes += theta(i,j+1)*V(i,j);
 		for (size_t j2=0; j2<n_components; ++j2){
-			term_cross_A += theta(i,j+1)*theta(i,j2+1)*C(j,j2);
+			term_cross_A += theta(i,j+1)*theta(i,j2+1)*W(j,j2);
 		}
 	}
 	ls_contrast_value_i += term_square + 2*theta(i,0)*term_cross_mu - 2*term_stieltjes + term_cross_A;
@@ -162,12 +105,12 @@ void ModelHawkesExpLeastSquaresSingle::compute_grad_i(const size_t i, const List
 	grad(i,0) = 2*(theta(i,0)*end_time - N[i]);
 
 	for (size_t j=0; j<n_components; ++j){
-		grad(i,0) += 2*theta(i,j+1)*H[j];
-		grad(i,j+1) = 2*theta(i,j+1)*H2[j] + 2*theta(i,j+1)*C(j,j) + 2*theta(i,0)*H[j] - 2*D(i,j);
+		grad(i,0) += 2*theta(i,j+1)*I[j];
+		grad(i,j+1) = 2*theta(i,j+1)*I2[j] + 2*theta(i,j+1)*W(j,j) + 2*theta(i,0)*I[j] - 2*V(i,j);
 
 		for (size_t j2=0; j2<n_components; ++j2){
 			if (j2 != j){
-				grad(i,j+1) += theta(i,j2+1)*(C(j,j2)+C(j2,j));
+				grad(i,j+1) += theta(i,j2+1)*(W(j,j2)+W(j2,j));
 			}
 		}
 	}
@@ -198,15 +141,15 @@ SharedArrayDouble2D ModelHawkesExpLeastSquaresSingle::compute_hessian(const List
 	hessian(0,0) = 2.*end_time;
 
 	for (size_t j=0; j<n_components; ++j){
-		hessian(0,j) = 2*H[j];
-		hessian(j,0) = 2*H[j];
+		hessian(0,j) = 2*I[j];
+		hessian(j,0) = 2*I[j];
 	}
 
 	for (size_t j=0; j<n_components; ++j){
 		for (size_t j2=0; j2<n_components; ++j2){
-			hessian(j+1,j2+1) = C(j,j2)+C(j2,j);
+			hessian(j+1,j2+1) = W(j,j2) + W(j2,j);
 			if (j==j2){
-				hessian(j+1,j2+1) += 2*H2[j];
+				hessian(j+1,j2+1) += 2*I2[j];
 			}
 		}
 	}
